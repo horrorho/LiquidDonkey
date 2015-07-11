3874a95d7801b77e7c2002028b330739db1aae1b
Ahseya <horrorho@users.noreply.github.com>
2015-07-11 22:48:52 +0100

    SimpleQueue + tests

---
+++ src/main/java/com/github/horrorho/liquiddonkey/util/pool/FifoLinkedListQueue.java
@@ -0,0 +1,121 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Ahseya.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.horrorho.liquiddonkey.util.pool;
+
+import java.util.NoSuchElementException;
+import net.jcip.annotations.NotThreadSafe;
+
+/**
+ * FIFO linked-list queue. Not synchronized.
+ *
+ * @author Ahseya
+ * @param <E> element type
+ */
+@NotThreadSafe
+public class FifoLinkedListQueue<E> implements SimpleQueue<E> {
+
+    private int count;
+    private Node<E> head;
+    private Node<E> tail;
+
+    FifoLinkedListQueue(int count, Node<E> head, Node<E> tail) {
+        this.count = count;
+        this.head = head;
+        this.tail = tail;
+    }
+
+    public FifoLinkedListQueue() {
+        this(0, null, null);
+    }
+
+    @Override
+    public FifoLinkedListQueue add(E e) {
+        Node node = new Node<>(e, null);
+
+        if (tail == null) {
+            tail = node;
+            head = node;
+
+        } else {
+            tail.setPrevious(node);
+            tail = node;
+        }
+        count++;
+        return this;
+    }
+
+    @Override
+    public E remove() {
+        if (count == 0) {
+            throw new NoSuchElementException("Empty queue");
+        }
+
+        E element = head.element();
+        head = head.previous();
+        if (head == null) {
+            tail = null;
+        }
+        count--;
+        return element;
+    }
+
+    @Override
+    public int size() {
+        return count;
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    static final class Node<E> {
+
+        private final E element;
+        private Node previous;
+
+        Node(E element, Node previous) {
+            this.element = element;
+            this.previous = previous;
+        }
+
+        public E element() {
+            return element;
+        }
+
+        public Node previous() {
+            return previous;
+        }
+
+        public Node setPrevious(Node next) {
+            this.previous = next;
+            return this;
+        }
+    }
+}
---
+++ src/test/java/com/github/horrorho/liquiddonkey/util/pool/FifoLinkedListQueueTest.java
@@ -0,0 +1,145 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Ahseya.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.horrorho.liquiddonkey.util.pool;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.NoSuchElementException;
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+/**
+ * FifoLinkedListQueueTest.
+ *
+ * @author Ahseya
+ */
+public class FifoLinkedListQueueTest {
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void testAddRemove() {
+        SimpleQueue<String> instance = new FifoLinkedListQueue<>();
+
+        instance.add("one");
+        assertThat(instance.remove(), is("one"));
+        instance.add("one");
+        instance.add("two");
+        assertThat(instance.remove(), is("one"));
+        assertThat(instance.remove(), is("two"));
+        instance.add("one");
+        instance.add("two");
+        assertThat(instance.remove(), is("one"));
+        instance.add("three");
+        assertThat(instance.remove(), is("two"));
+        assertThat(instance.remove(), is("three"));
+    }
+
+    @Test
+    public void testRemoveException() {
+        SimpleQueue<String> instance = new FifoLinkedListQueue<>();
+
+        exception.expect(NoSuchElementException.class);
+        instance.remove();
+    }
+
+    @Test
+    public void testAddRemoveException() {
+        SimpleQueue<String> instance = new FifoLinkedListQueue<>();
+
+        instance.add("one");
+        instance.remove();
+        exception.expect(NoSuchElementException.class);
+        instance.remove();
+    }
+
+    @Test
+    public void testSize() {
+        SimpleQueue<String> instance = new FifoLinkedListQueue<>();
+
+        assertThat(instance.size(), is(0));
+        instance.add("one");
+        assertThat(instance.size(), is(1));
+        instance.remove();
+        assertThat(instance.size(), is(0));
+        instance.add("one");
+        assertThat(instance.size(), is(1));
+    }
+
+    @Test
+    public void testIsFull() {
+        SimpleQueue<String> instance = new FifoLinkedListQueue<>();
+
+        assertThat(instance.isFull(), is(false));
+        instance.add("one");
+        assertThat(instance.isFull(), is(false));
+    }
+
+    @Test
+    public void testIsEmpty() {
+        SimpleQueue<String> instance = new FifoLinkedListQueue<>();
+
+        assertThat(instance.isEmpty(), is(true));
+        instance.add("one");
+        assertThat(instance.isEmpty(), is(false));
+        instance.remove();
+        assertThat(instance.isEmpty(), is(true));
+        instance.add("one");
+        assertThat(instance.isEmpty(), is(false));
+    }
+
+    @Test
+    public void testAddAll() {
+        SimpleQueue<String> instance = new FifoLinkedListQueue<>();
+
+        Collection<String> collection = Arrays.asList("one", "two", "three");
+        instance.addAll(collection);
+
+        assertThat(instance.size(), is(3));
+        assertThat(instance.isFull(), is(false));
+        assertThat(instance.remove(), is("one"));
+        assertThat(instance.remove(), is("two"));
+        assertThat(instance.remove(), is("three"));
+        assertThat(instance.isEmpty(), is(true));
+    }
+
+    @Test
+    public void testContains() {
+        SimpleQueue<String> instance = new FifoArrayQueue<>(3);
+
+        assertThat(instance.contains("one"), is(false));
+        instance.add("one");
+        assertThat(instance.contains("one"), is(true));
+        instance.add("two");
+        assertThat(instance.contains("one"), is(true));
+        assertThat(instance.remove(), is("one"));
+        assertThat(instance.contains("one"), is(false));
+        assertThat(instance.remove(), is("two"));
+        assertThat(instance.isEmpty(), is(true));
+    }
+}
