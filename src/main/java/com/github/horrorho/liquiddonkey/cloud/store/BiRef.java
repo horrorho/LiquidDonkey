a6354695d5815414091aac3c37718f179a3760c8
Ahseya <horrorho@users.noreply.github.com>
2015-07-17 06:27:05 +0100

    Obsolete test method.

--- src/main/java/com/github/horrorho/liquiddonkey/cloud/store/BiRef.java
+++ src/main/java/com/github/horrorho/liquiddonkey/cloud/store/BiRef.java
@@ -75,35 +75,6 @@ public class BiRef<K, V> {
         return new BiRef(kToVSet, vToKSet);
     }
 
-    public static <K, V> BiRef<K, V> fromx(Map<K, Collection<V>> map) {
-
-        ConcurrentMap<K, Set<V>> kToVSet = new ConcurrentHashMap<>();
-        ConcurrentMap<V, Set<K>> vToKSet = new ConcurrentHashMap<>();
-
-        map.entrySet().stream()
-                .peek(entry -> {
-                    if (entry.getKey() == null || entry.getValue() == null || entry.getValue().contains(null)) {
-                        throw new IllegalArgumentException("Null values not permitted");
-                    }
-                })
-                .filter(entry -> !entry.getValue().isEmpty())
-                .forEach(entry -> {
-                    K key = entry.getKey();
-
-                    Set<V> set = Collections.newSetFromMap(new ConcurrentHashMap<>());
-
-                    entry.getValue().stream().forEach(value -> {
-                        set.add(value);
-                        vToKSet.computeIfAbsent(value, k
-                                -> Collections.newSetFromMap(new ConcurrentHashMap<>())).add(key);
-                    });
-
-                    kToVSet.put(key, set);
-                });
-
-        return new BiRef(kToVSet, vToKSet);
-    }
-
     private final ConcurrentMap<K, Set<V>> kToVSet;    // Requires concurrent Set
     private final ConcurrentMap<V, Set<K>> vToKSet;    // Requires concurrent Set
 
