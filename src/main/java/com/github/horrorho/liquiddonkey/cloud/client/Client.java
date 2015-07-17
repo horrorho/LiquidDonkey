7d4253a9c7416c463fd1b5e0cfbfb3e048b08488
Ahseya <horrorho@users.noreply.github.com>
2015-07-17 06:28:07 +0100

    Imports.

--- src/main/java/com/github/horrorho/liquiddonkey/cloud/client/Client.java
+++ src/main/java/com/github/horrorho/liquiddonkey/cloud/client/Client.java
@@ -37,7 +37,6 @@ import com.google.protobuf.ByteString;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
