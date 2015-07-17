7861515e41da2562563b4e6e1ef7d6e2da90380e
Ahseya <horrorho@users.noreply.github.com>
2015-07-17 06:08:59 +0100

    Now writes.

--- src/main/java/com/github/horrorho/liquiddonkey/cloud/donkey/WriterDonkey.java
+++ src/main/java/com/github/horrorho/liquiddonkey/cloud/donkey/WriterDonkey.java
@@ -23,17 +23,23 @@
  */
 package com.github.horrorho.liquiddonkey.cloud.donkey;
 
+import com.github.horrorho.liquiddonkey.cloud.file.WriterResult;
 import com.github.horrorho.liquiddonkey.cloud.protobuf.ChunkServer;
+import com.github.horrorho.liquiddonkey.cloud.protobuf.ICloud;
 import com.github.horrorho.liquiddonkey.cloud.store.StoreManager;
+import com.github.horrorho.liquiddonkey.cloud.store.StoreWriter;
 import com.github.horrorho.liquiddonkey.exception.BadDataException;
+import com.github.horrorho.liquiddonkey.printer.Level;
+import com.github.horrorho.liquiddonkey.printer.Printer;
 import com.github.horrorho.liquiddonkey.util.pool.Release;
+import com.google.protobuf.ByteString;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 import net.jcip.annotations.NotThreadSafe;
-import org.bouncycastle.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -48,12 +54,16 @@ public final class WriterDonkey extends Donkey {
     private static final Logger log = LoggerFactory.getLogger(WriterDonkey.class);
 
     private final StoreManager manager;
+    private final Printer printer;
     private final Function<WriterDonkey, FetchDonkey> fetchDonkeys;
+    private final Function<Map<ByteString, StoreWriter>, DonkeyWriter> donkeyWriters;
     private byte[] data;
 
-    public WriterDonkey(
+    WriterDonkey(
             StoreManager manager,
+            Printer printer,
             Function<WriterDonkey, FetchDonkey> fetchDonkeys,
+            Function<Map<ByteString, StoreWriter>, DonkeyWriter> donkeyWriters,
             byte[] data,
             ChunkServer.StorageHostChunkList chunkList,
             List<Exception> exceptions,
@@ -63,7 +73,9 @@ public final class WriterDonkey extends Donkey {
         super(chunkList, exceptions, retryCount, fatal);
 
         this.manager = Objects.requireNonNull(manager);
+        this.printer = Objects.requireNonNull(printer);
         this.fetchDonkeys = Objects.requireNonNull(fetchDonkeys);
+        this.donkeyWriters = Objects.requireNonNull(donkeyWriters);
         this.data = Objects.requireNonNull(data);
     }
 
@@ -72,9 +84,15 @@ public final class WriterDonkey extends Donkey {
         log.trace("<< toProcess()");
 
         Release<Track, Donkey> toDo;
-        try {
-            manager.put(chunkList, data);
-            data = null;
+
+        try (DonkeyWriter writer = donkeyWriters.apply(manager.put(chunkList, data))) {
+            writer.write().
+                    entrySet().stream().forEach(
+                            entry -> printer.println(Level.VV,
+                                    "\t" + entry.getKey().getDomain()
+                                    + " " + entry.getKey().getRelativePath()
+                                    + " " + entry.getValue()));
+
             toDo = Release.dispose();
         } catch (BadDataException ex) {
             log.warn("-- toProcess() > exception: ", ex);
