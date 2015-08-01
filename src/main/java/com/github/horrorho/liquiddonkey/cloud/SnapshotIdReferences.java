9222b50db079a0df8e8456958217dbf1f9da7081
Ahseya <horrorho@users.noreply.github.com>
2015-08-01 03:20:36 +0100

    Imports.

--- src/main/java/com/github/horrorho/liquiddonkey/cloud/SnapshotIdReferences.java
+++ src/main/java/com/github/horrorho/liquiddonkey/cloud/SnapshotIdReferences.java
@@ -23,7 +23,6 @@
  */
 package com.github.horrorho.liquiddonkey.cloud;
 
-import com.github.horrorho.liquiddonkey.cloud.data.Backup;
 import com.github.horrorho.liquiddonkey.cloud.protobuf.ICloud;
 import java.util.Comparator;
 import java.util.HashMap;
