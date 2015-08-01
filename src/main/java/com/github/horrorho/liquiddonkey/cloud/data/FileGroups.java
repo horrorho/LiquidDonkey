ad1cd318d19ea796667ff5eb2c6c516dec7ffc8b
Ahseya <horrorho@users.noreply.github.com>
2015-08-01 16:56:00 +0100

    Reassigned duties between FilesGroups and client

--- src/main/java/com/github/horrorho/liquiddonkey/cloud/data/FileGroups.java
+++ src/main/java/com/github/horrorho/liquiddonkey/cloud/data/FileGroups.java
@@ -34,7 +34,6 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import net.jcip.annotations.Immutable;
 import net.jcip.annotations.ThreadSafe;
