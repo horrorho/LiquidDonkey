a2f440e37ef18c2fc39ad03df042225e19ef5ca0
Ahseya <horrorho@users.noreply.github.com>
2015-08-07 13:45:08 +0100

    Bugfix for introduced bad timing.

--- src/main/java/com/github/horrorho/liquiddonkey/cloud/outcome/OutcomesProgress.java
+++ src/main/java/com/github/horrorho/liquiddonkey/cloud/outcome/OutcomesProgress.java
@@ -66,25 +66,38 @@ public final class OutcomesProgress implements Consumer<Map<ICloud.MBSFile, Outc
     @GuardedBy("lock")
     private int tick;
     @GuardedBy("lock")
+    private long ticksMs;
+    @GuardedBy("lock")
     private int percentage;
     @GuardedBy("lock")
     private long ms;
     @GuardedBy("lock")
     private final StringBuffer sb = new StringBuffer();
 
-    OutcomesProgress(long totalBytes, long tickDelta, Printer out, Lock lock, long bytes, int tick, int percentage, long ms) {
+    OutcomesProgress(
+            long totalBytes,
+            long tickDelta,
+            Printer out,
+            Lock lock,
+            long bytes,
+            int tick,
+            long ticksMs,
+            int percentage,
+            long ms) {
+
         this.totalBytes = totalBytes;
         this.tickDelta = tickDelta;
         this.out = Objects.requireNonNull(out);
         this.lock = Objects.requireNonNull(lock);
         this.bytes = bytes;
         this.tick = tick;
+        this.ticksMs = ticksMs;
         this.percentage = percentage;
         this.ms = ms;
     }
 
     public OutcomesProgress(long totalBytes, long tickDelta, Printer out, boolean fair) {
-        this(totalBytes, tickDelta, out, new ReentrantLock(fair), 0, 0, 0, System.currentTimeMillis());
+        this(totalBytes, tickDelta, out, new ReentrantLock(fair), 0, 0, 0, 0, System.currentTimeMillis());
     }
 
     @Override
@@ -134,6 +147,7 @@ public final class OutcomesProgress implements Consumer<Map<ICloud.MBSFile, Outc
             return;
         }
 
+        ticksMs += deltaMs;
         if (tick == 0) {
             sb.append(String.format("%2s", percentage)).append("% .");
             out.print("\r" + sb.toString());
@@ -143,7 +157,8 @@ public final class OutcomesProgress implements Consumer<Map<ICloud.MBSFile, Outc
         } else if (tick == 39) {
             long rate = deltaMs == 0
                     ? -1
-                    : tickDelta * 40000 / (deltaMs * 1024);
+                    : tickDelta * 40000 / (ticksMs * 1024);
+            ticksMs = 0;
 
             sb.append('.').append(String.format("%6s", rate)).append(" KiB/s");
             out.print("\r" + sb.toString());
